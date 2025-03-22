95c4b8c0b8cc0bed10dd479379b12d03354932a7
95ayman <aymandzfr@gmail.com>
2025-03-22 17:15:41 +0100

    Update login.php

--- php/login.php
+++ php/login.php
@@ -56,7 +56,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	  </div>
 		<div class="menu">
 			<ul class="navi-liens">
-            <li><a href="../index.html" class="navi-lien">ACCUEIL</a></li>
+            <li><a href="index.php" class="navi-lien">ACCUEIL</a></li>
             <li><a href="destinations.php" class="navi-lien">DESTINATIONS</a></li>
             <li><a href="search.php" class="navi-lien">RECHERCHE</a></li>
             <li><a href="about.php" class="navi-lien">QUI SOMMES NOUS ?</a></li>          
@@ -106,7 +106,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	 <img src="../style/Image/logoD.png" alt="logo">
 		<div class="footer-container">
 			<h3>Liens Utiles</h3>
-			  <p><a href="../index.html">Accueil</a></p>
+			  <p><a href="index.php">Accueil</a></p>
 			  <p><a href=".destinations.php">Destinations</a></p>
 			  <p><a href="search.php">Recherche</a></p>
 			  <p><a href="signup.php">Inscription</a></p>
@@ -126,4 +126,5 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	
 </body>
 </html>
+
 
