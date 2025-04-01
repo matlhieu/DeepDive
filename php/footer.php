203a47206cbc3bda5eda67aab25b60daa1b022d0
Mathieu <166537907+Sparthuus@users.noreply.github.com>
2025-04-01 14:32:30 +0200

    Add footer and navbar in php

---
+++ php/footer.php
@@ -0,0 +1,27 @@
+<footer>
+ <div class="footer-main">
+	 <img src="../style/Image/logoD.png" alt="logo">
+		<div class="footer-container">
+			<h3>Liens Utiles</h3>
+            <?php 
+              if (isset($_SESSION['role']) && $_SESSION['role'] === 'Admin'){
+                 echo'<p><a href="admin.php">Admin</a></p>';
+              }
+               ?>
+			  <p><a href="../index.php">Accueil</a></p>
+			  <p><a href="destinations.php">Destinations</a></p>
+			  <p><a href="search.php">Recherche</a></p>
+			  <p><a href="signup.php">Inscription</a></p>
+			  <p><a href="login.php">Connexion</a></p>
+		</div>
+	
+		<div class="footer-container">
+			<h3>Nous contacter</h3>
+			<p><a href="about.php">En savoir plus sur nous</a><p>
+			<p>Téléphone : +33 6 45 15 25</p>
+			<p>Adresse : 75421, Varay, 22 Place d'Elden</p>
+			<p>deepdive05@gmail.com</p>
+		</div>
+	</div>
+	<p> © 2025 DeepDive | All rights reserved </p>
+</footer>
