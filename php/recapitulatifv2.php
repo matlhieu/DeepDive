6c195f0dd4eff2abf34e29bdc682fdda200cf6dc
Mathieu <166537907+matlhieu@users.noreply.github.com>
2025-04-26 18:40:24 +0200

    Update recapitulatifv2.php

--- php/recapitulatifv2.php
+++ php/recapitulatifv2.php
@@ -1,10 +1,10 @@
 <?php
 
 session_start();
- if (!isset($_SESSION['role'] || isset($_SESSION['role'] === 'ban'){		
- 	header('Location: login.php');
- 	exit();
- }
+ if (!isset($_SESSION['role']) || $_SESSION['role'] === 'ban') {		
+    header('Location: login.php');
+    exit();
+}
 
 date_default_timezone_set('Europe/Paris');
 $voyages_json = file_get_contents("voyagesv2.json");
