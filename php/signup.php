7917ea7401630a0af684795c5d98dd9fea8f32c6
marc-ant0ine <ma.abale10@gmail.com>
2025-05-17 17:10:28 +0200

    Update signup.php

--- php/signup.php
+++ php/signup.php
@@ -135,7 +135,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
        </div>
    </section>
    <?php include("footer.php") ?>
-<script src="../js/validation.js"></script>
+<script src="../js/verification.js"></script>
 </body>
 </html>
 
